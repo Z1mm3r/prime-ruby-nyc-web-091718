a3c735e108a544b2144cd539cf5e0a43d3131e04
Casey Zimmer <czimmer@ramapo.edu>
2018-09-13 11:22:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,31 @@
 # Add  code here!
+
+def prime?(number)
+  
+  possible_factors = []
+  
+  if number == 1 
+    return false
+  
+  elsif number > 0
+  halfway = (number / 2) + 1
+  possible_factors = (2..halfway).to_a
+  
+  elsif number < 0
+  return FALSE
+  
+  else
+    return FALSE
+  end
+  
+  while(possible_factors.size > 0)
+    if( number != possible_factors[possible_factors.size - 1] && number % possible_factors[possible_factors.size - 1] == 0 )
+      return FALSE
+    else
+     possible_factors.pop
+    end
+  end
+  return TRUE
+  
+  
+end
