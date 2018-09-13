1da51d83b5c7304212111cf6a678a024a916369d
Casey Zimmer <czimmer@ramapo.edu>
2018-09-13 11:14:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,28 @@
 # Add  code here!
 
 def prime?(number)
+  
+  possible_factors = []
+  
+  if number > 0
   halfway = (number / 2) + 1
-  count  = 0
-  if(number > 0)
-    until 
+  possible_factors = 2..halfway
+  
+  elsif number < 0
+  halfway = (number / 2) - 1
+  possible_factors = halfway..-2
+  
+  else
+    return FALSE
+  end
+  
+  while(possible_factors.size > 0)
+    if( number != possible_factors[possible_factors.size -1] && number % possible_factors[possible_factors.size -1] == 0 )
+      return FALSE
+    else
+     possible_factors.pop
+    end
+end
+  
+  
 end
